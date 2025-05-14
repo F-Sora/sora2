f85d852d8ee3a1e2c205af028f8d6e8ba83818a2
Sora0 <sokkuru0924@docomo.ne.jp>
2025-05-14 12:04:20 +0900

    5/14

--- app/src/main/java/jp/ac/gifu_u/sora/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/sora/myapplication/MainActivity.java
@@ -1,20 +1,56 @@
 package jp.ac.gifu_u.sora.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private LocationManager manager;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
-        setContentView(new MyView(this));
-        // Button b = (Button) findViewById(R.id.button1);
-        // b.setOnClickListener(this);
+        setContentView(R.layout.activity_main);
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // パーミッションチェック
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+
+        manager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 1000, 10, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            manager.removeUpdates(this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        Toast.makeText(this,
+                String.format("%.3f %.3f", lat, lng),
+                Toast.LENGTH_SHORT).show();
     }
 }
